c2833e4d7454089365f085ec544db0b50dec0936
CodyA1117 <adamscody079@gmail.com>
2025-04-08 06:20:28 -0500

    corrected a logic error within the loop, and re ordered to get the correct function of doubling the es and the us to make a whale sound.

--- main.js
+++ main.js
@@ -1,22 +1,20 @@
-let input = "Hello world, thanks you for the fish";
+let input = "turpentine and turtles";
 const vowels = ["a", "e", "i", "o", "u"];
 let resultArray = [];
 
 for (let i = 0; i < input.length; i++) {
-  if (input[i] === "e" || input[i] === "u") {
-    resultArray.push(input[i]);
-  }
-  // console.log(input[i]);
-  for (let j = 0; j < vowels.length; j++) {
-    //
-    if (input[i] === vowels[j]) {
-      console.log(input[i]);
-      resultArray.push(input[i]);
-      // console.log(resultArray);
+    for (let j = 0; j < vowels.length; j++) {
+      if (input[i] === vowels[j]) {
+        // Double 'e' and 'u'
+        if (input[i] === 'e' || input[i] === 'u') {
+          resultArray.push(input[i]);
+        }
+        resultArray.push(input[i]);
+      }
     }
   }
+      // console.log(resultArray);
 
-}
 
 
 console.log(resultArray);
